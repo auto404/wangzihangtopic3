e9558fc7ad9deaa9a378605b999e0e214d7b76fe
auto404 <3515884133@qq.com>
2022-07-01 20:24:34 +0800

    first

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(60, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(231, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "请输入平面点对的个数n";
+            this.label1.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(319, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 31);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(540, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 37);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "开始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 21;
+            this.listBox1.Location = new System.Drawing.Point(49, 75);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(438, 634);
+            this.listBox1.TabIndex = 3;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged_1);
+            // 
+            // chart1
+            // 
+            chartArea1.CursorX.SelectionEnd = -100D;
+            chartArea1.CursorX.SelectionStart = 100D;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(540, 92);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+            series1.Legend = "Legend1";
+            series1.Name = "平面最接近点";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(1712, 763);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2342, 1136);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "平面最接近点";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public static int INFINITE_DISTANCE = 65535; // 无限大距离
+        public static double COORDINATE_RANGE = 100.0;// 横纵坐标范围为[-100,100]
+        public Point a1, b1, a2, b2;              //保存分割后两个子集中最小点对
+        public Point a, b;            //最近点对
+        public class Point
+        {// 二维坐标上的点Point
+            public double x;
+            public double y;
+        }
+        class XComparer : IComparer<Point>
+        {
+            //实现升序
+
+            public int Compare(Point x, Point y)
+            {
+                return (x.x.CompareTo(y.x));
+            }
+        }
+        class YComparer : IComparer<Point>
+        {
+            //实现升序
+
+            public int Compare(Point x, Point y)
+            {
+                return (x.y.CompareTo(y.y));
+            }
+        }
+        double Distance(Point a, Point b)
+        {//平面上任意两点对之间的距离公式计算
+            return Math.Sqrt((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y));
+        }
+
+        double ClosestPair(List<Point> points, int length, ref Point a, ref Point b)
+        {// 求出最近点对记录，并将两点记录再a、b中
+            double distance;                   //记录集合points中最近两点距离 
+            double d1, d2;                     //记录分割后两个子集中各自最小点对距离
+            int i = 0, j = 0, k = 0, x = 0;
+
+            if (length < 2)
+                return INFINITE_DISTANCE;    //若子集长度小于2，定义为最大距离，表示不可达
+            else if (length == 2)
+            {//若子集长度等于2，直接返回该两点的距离
+                a = points[0];
+                b = points[1];
+                distance = Distance(points[0], points[1]);
+            }
+            else
+            {//子集长度大于3，进行分治求解
+                List<Point> pts1 = new List<Point>();   //开辟两个子集
+                List<Point> pts2 = new List<Point>();
+
+                points.Sort(new XComparer());    //调用algorithm库中的sort函数对points进行排序，compareX为自定义的排序规则
+                double mid = points[(length - 1) / 2].x;    //排完序后的中间下标值，即中位数
+
+                for (i = 0; i < length / 2; i++)
+                    pts1.Add(points[i]);
+                for (j = 0, i = length / 2; i < length; i++)
+                    pts2.Add(points[i]);
+
+                d1 = ClosestPair(pts1, length / 2, ref a1, ref b1);             //分治求解左半部分子集的最近点  
+                d2 = ClosestPair(pts2, length - length / 2, ref a2, ref b2);    //分治求解右半部分子集的最近点  
+
+                if (d1 < d2) { distance = d1; a = a1; b = b1; }            //记录最近点，最近距离
+                else { distance = d2; a = a2; b = b2; }
+
+                //merge - 进行子集合解合并
+                //求解跨分割线并在δ×2δ区间内的最近点对
+                List<Point> pts3 = new List<Point>();
+
+                for (i = 0, k = 0; i < length; i++)                        //取得中线2δ宽度的所有点对共k个    
+                    if (Math.Abs(points[i].x - mid) <= distance)
+                        pts3.Add(points[i]);
+                pts3.Sort(new YComparer());                                    // 以y排序矩形阵内的点集合
+
+                for (i = 0; i < k; i++)
+                {
+                    if (pts3[j].x - mid >= 0)                                             // 只判断左侧部分的点
+                        continue;
+                    x = 0;
+                    for (j = i + 1; j <= i + 6 + x && j < k; j++)            //只需与有序的领接的的6个点进行比较
+                    {
+                        if (pts3[j].x - mid < 0)
+                        {//  假如i点是位于mid左边则只需判断在mid右边的j点即可
+                            x++;
+                            continue;
+                        }
+                        if (Distance(pts3[i], pts3[j]) < distance)
+                        {//如果跨分割线的两点距离小于已知最小距离，则记录该距离和两点
+                            distance = Distance(pts3[i], pts3[j]);
+                            a = pts3[i];
+                            b = pts3[j];
+                        }
+                    }
+                }
+            }
+            return distance;
+        }
+        void SetPoints(ref List<Point> points, int length)
+        {//随机函数对点数组points中的二维点进行初始化
+
+
+
+            Random rd = new Random();
+            for (int i = 0; i < length; i++)
+            {
+                Point p = new Point();
+                p.x = (rd.Next() % COORDINATE_RANGE * 200) / COORDINATE_RANGE - COORDINATE_RANGE;
+                p.y = (rd.Next() % COORDINATE_RANGE * 200) / COORDINATE_RANGE - COORDINATE_RANGE;
+                points.Add(p);
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+
+
+        }
+
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+            double diatance;    //点对距离
+            chart1.Series[0].Points.Clear();
+            int num = int.Parse(textBox1.Text.Trim());
+            if (num < 2)
+            {
+                listBox1.Items.Add("请输入大于等于2的点个数！！");
+                listBox1.Items.Clear();
+            }
+
+            else
+            {
+                listBox1.Items.Add("随机生成的" + num + "个二维点对如下：");
+                List<Point> points = new List<Point>();
+             
+                SetPoints(ref points, num);
+                for (int i = 0; i < num; i++)
+                {
+                    chart1.Series[0].Points.AddXY(points[i].x, points[i].y);
+                    listBox1.Items.Add("(" + points[i].x + "," + points[i].y + ")");
+                }
+
+                diatance = ClosestPair(points, num, ref a, ref b);
+                listBox1.Items.Add("按横坐标排序后的点对:");
+                for (int i = 0; i < num; i++)
+                    listBox1.Items.Add("(" + points[i].x + "," + points[i].y + ")");
+                listBox1.Items.Add("最近点对为：");
+                listBox1.Items.Add("(" + a.x + "," + a.y + ")和(" + b.x + "," + b.y + ")");
+                listBox1.Items.Add("最近点对距离为：" + diatance);
+
+            }
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+     
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
